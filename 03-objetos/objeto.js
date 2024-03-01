d2fb87c72a17e8ee077f4aaa01b764a830d830e8
Jose <joseantonio.garcia@campusfp.es>
2024-03-01 10:05:05 +0100

    “Creando objeto animal”,

---
+++ 03-objetos/objeto.js
@@ -0,0 +1,20 @@
+
+// Objeto que describe un perro
+const perro = {
+  nombre: "Toby",
+  tipo: "Perro",
+  habitat: "Doméstico",
+  edad: 5,
+  color: "Marron"
+};
+
+// Mostrar todos los valores del objeto en la consola
+console.log("Objeto perro:", perro);
+
+// Cambiar la edad y el color del perro
+perro.edad = 7;
+perro.color = "Negro";
+
+// Mostrar la edad y el color actualizados en la consola
+console.log("Edad:", perro.edad);
+console.log("Color:", perro.color);
