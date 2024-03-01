509fe9b70144d2b0e80654bca66e9bcdefa2ad24
Jose <joseantonio.garcia@campusfp.es>
2024-03-01 10:27:34 +0100

    “Creando objeto animal”,

---
+++ 05-Funciones/funciones.js
@@ -0,0 +1,22 @@
+// Función para sumar dos números
+function suma(a, b) {
+    return a + b;
+  }
+  
+  // Función para restar dos números
+  function resta(a, b) {
+    return a - b;
+  }
+  
+  // Variable que guarda el resultado de la suma
+  const resultadoSuma = suma(23, 46);
+  
+  // Variable que guarda el resultado de la resta
+  const resultadoResta = resta(64, 23);
+  
+  // Mostrar el resultado de la suma en la consola
+  console.log("Resultado de la suma:", resultadoSuma);
+  
+  // Mostrar el resultado de la resta en la consola
+  console.log("Resultado de la resta:", resultadoResta);
+  
