3ef6bfc6aacf0dce5c5da63fa7d0ac9c30d6644c
Jose <joseantonio.garcia@campusfp.es>
2024-03-01 09:51:27 +0100

    ejercicio

---
+++ 02-Variables/script.js
@@ -0,0 +1,9 @@
+// Declaración de variables
+let nombre = "Tu nombre";
+let apellidos = "Tus apellidos";
+let animal = "Tu animal preferido (o más odiado)";
+
+// Visualización de variables en el navegador
+console.log("Nombre:", nombre);
+console.log("Apellidos:", apellidos);
+console.log("Animal:", animal);
